98d06db7c2467e406753876e9938e86aa029fe23
Hemantkumar Panchal <panchalhemant2001@yahoo.com>
2018-04-17 07:54:24 +0000

    secrets.js added to gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,22 @@
 var request = require('request');
+var ght = require("./secrets.js");
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'user-agent': 'request',
+     'Authorization': `token ${ght.GITHUB_TOKEN}`
+    }
+
+
+  };
+
+
+  request(options, function(err, res, body) {
     cb(err, body);
   });
 }
@@ -15,3 +27,9 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+
+
+/*
+curl -i -H 'Authorization: token 9f4c611902ab705a7acf58dfd7d1e8dd1fa611c9' https://api.github.com/repos/jquery/jquery/contributors
+*/
